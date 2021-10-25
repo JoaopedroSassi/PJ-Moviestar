daa97fa058bc8f7b2add17adccd68eb7d6fe7ee3
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-10-25 18:39:55 -0300

    Add editprofile.php

---
+++ editprofile.php
@@ -0,0 +1,11 @@
+<?php
+   include_once('templates/header.php');
+?>
+
+   <div id="main-container" class="container-fluid">
+      <h1>Editar perfil</h1>
+   </div>
+
+<?php
+   include_once('templates/footer.php');
+?>
