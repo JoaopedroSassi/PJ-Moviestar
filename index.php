6603ac470252cf4d23474621bd22d9ccbbf222d0
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-11-05 16:54:11 -0300

    Upt index + add movie_card

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
-   include_once('templates/header.php');
+   require_once('templates/header.php');
+   require_once('dao/MoviesDAO.php');
+
+   $moviesDao = new MovieDAO($conn, $BASE_URL);
+   
+   $latestMovies = $moviesDao->getLatestMovies();
+   $fantasyMovies = [];
+   $actionMovies = [];
 ?>
+
    <div id="main-container" class="container-fluid">
       <h2 class="section-title">Filmes novos</h2>
-      <p class="section-description">Veja as críticas dos últimos filmes adicionados no <span class="emphasis-text">MovieStar</span> </p>
+      <p class="section-description">Veja as críticas dos últimos filmes adicionados no <span class="emphasis-text">MovieStar</span></p>
       <div class="movies-container">
-         <div class="card movie-card">
-            <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/movie_cover.jpg');"></div>
-            <div class="card-body">
-               <p class="card-rating">
-                  <i class="bi bi-star"></i>
-                  <span class="rating">9</span>
-               </p>
-               <h5 class="card-title">
-                  <a href="#">Título do filme</a>
-               </h5>
-               <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
-               <a href="#" class="btn btn-primary card-btn">Conhecer</a>
-            </div>
-         </div>
+         <?php foreach ($latestMovies as $movie): ?>
+            <?php require('templates/movie_card.php'); ?>
+         <?php endforeach; ?>
       </div>
 
       <h2 class="section-title">Fantasia</h2>
