82350b4602e45bef4ed536321d52af23885402c4
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-11-06 23:33:19 -0300

    Upt "utf-8" config

--- connection.php
+++ connection.php
@@ -5,7 +5,9 @@
    $db_user = "root";
    $db_pass = "123456";
 
+   ini_set('default_charset', 'UTF-8');
    $conn = new PDO("mysql:dbname=". $db_name .";host=". $db_host, $db_user, $db_pass);
+   $conn->query("SET NAMES utf8");
 
    //Errors PDO
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
